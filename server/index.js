3b7492f0b86b580e35639173f4cd497138b2019f
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-17 22:58:41 +0100

    enabled mongoose feature

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
 
 
 // CONFIGURATION
@@ -24,3 +27,13 @@ app.use("/client", clientRoutes)
 app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        app.listen(PORT, () => HTMLFormControlsCollection.log(`Server Port: ${PORT}`))
+    }).catch((error) => console.log(`${error} did not connect`));
