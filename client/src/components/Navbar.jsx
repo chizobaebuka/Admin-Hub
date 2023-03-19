0db5727710dc1ac31d697c0a99cab76c7cc66d4e
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-19 09:44:28 +0100

    modified the navbar and made it responsive onclick to open and close the sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,10 @@ import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
 
 
 
-const Navbar = () => {
+const Navbar = ({
+    isSidebarOpen,
+    setIsSidebarOpen,
+}) => {
     const dispatch = useDispatch();
     const theme = useTheme();
 
@@ -23,7 +26,7 @@ const Navbar = () => {
         <Toolbar sx={{ justifyContent: "space-between"}} >
             {/* {LEFT SIDE} */}
             <FlexBetween>
-                <IconButton onClick={() => console.log('open/close sidebar')}>
+                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                     <MenuIcon />
                 </IconButton>
                 <FlexBetween
