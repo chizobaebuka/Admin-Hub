79e23c0c82d6c3ab5c249bd27c4a4bae0bc55c60
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-21 12:25:50 +0100

    getting all the products and combining it with the product stats

---
+++ server/controllers/client.js
@@ -0,0 +1,23 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async(req, res) => {
+    try {
+        const products = await Product.find();
+
+        const productsWitchStats = await Promise.all(
+            products.map(async (product) => {
+                const stat = await ProductStat.find({
+                    productId: product._id
+                })
+                return {
+                    ...product._doc,
+                    stat,
+                }
+            })
+        )
+    } catch (error){
+        res.status(404).json({ message: error.message });
+    }
+}
+
