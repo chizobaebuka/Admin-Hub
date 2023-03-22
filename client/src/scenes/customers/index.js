743eb22e0b087298519c68173a6429b0f0890e5c
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-22 16:22:00 +0100

    modified and completed

--- client/src/scenes/customers/index.js
+++ client/src/scenes/customers/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, useTheme } from "@mui/material";
 import { useGetCustomersQuery } from "state/api"
 import Header from 'components/Header';
-import { DataGrid } from "@mui/x-data-grid"
+import { DataGrid } from "@mui/x-data-grid";
 
 const Customers = () => {
     const theme = useTheme();
