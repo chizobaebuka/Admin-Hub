7c007b8351f0cafee02f73a67a51617cf3521025
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-24 17:15:14 +0100

    creating routes and themes for app pages

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,14 @@ import Dashboard from "scenes/dashboard";
 import Layout from "scenes/Layout";
 import Products from "scenes/products";
 import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+import Geography from "scenes/geography";
 
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme =useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  
   return <div className="app">
     <BrowserRouter>
       <ThemeProvider theme={theme}>
@@ -23,6 +26,8 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/customers" element={<Customers />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/geography" element={<Geography />} />
           </Route>
         </Routes>
       </ThemeProvider>
