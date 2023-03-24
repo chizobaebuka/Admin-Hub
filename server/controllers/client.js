1fa9da28da1aa0d57b74cbb8617dd428c16424d3
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-24 17:07:25 +0100

    Getting the number of users per country visited in the Geography file

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,8 +2,9 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import Transaction from "../models/Transaction.js";
 import User from '../models/User.js';
+import getCountryIso3 from "country-iso-2-to-3";
 
-
+// PRODUCTS
 export const getProducts = async(req, res) => {
     try {
         const products = await Product.find();
@@ -25,6 +26,7 @@ export const getProducts = async(req, res) => {
     }
 };
 
+// CUSTOMERS
 export const getCustomers = async(req, res) => {
     try{
         const customers = await User.find({ role: "user" }).select("-password");
@@ -35,12 +37,13 @@ export const getCustomers = async(req, res) => {
 };
 
 
+// TRANSACTIONS
 export const getTransactions = async(req, res) => {
     try{
-        // sort should have these: {" field", "userId", "sort": "desc"}
-        const { page = 1, pageSize = 20, sort = null, search = ""} = req.query;
+        // sort should have these { "field", "userId", "sort": "desc" }
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
 
-        // formatting sort should look like { userId: -1 }
+        // Formatted sort should look like { userId: -1 }
         const generateSort = () => {
             const sortParsed = JSON.parse(sort);
             const sortFormatted = {
@@ -54,9 +57,9 @@ export const getTransactions = async(req, res) => {
 
         const transactions = await Transaction.find({
             $or: [
-                { cost: { $regex: new RegExp(search, "i") } },
-                { userId: { $regex: new RegExp(search, "i") } },
-            ], 
+                { cost: { $regex: new RegExp(search, "i") } }, 
+                { userId: { $regex: new RegExp(search, "i") } }, 
+            ],
         })
             .sort(sortFormatted)
             .skip(page * pageSize)
@@ -68,7 +71,7 @@ export const getTransactions = async(req, res) => {
 
         res.status(200).json({
             transactions,
-            total
+            total,
         });
     } catch (error){
         res.status(404).json({ message: error.message })
@@ -76,3 +79,28 @@ export const getTransactions = async(req, res) => {
 };
 
 
+// GEOGRAPHY
+export const getGeography = async(req, res) => {
+    try {
+       const users = await User.find();
+
+       const mappedLocations = users.reduce((acc, { country }) => {
+        const countryISO3 = getCountryIso3(country);
+        if (!acc[countryISO3]){
+            acc[countryISO3] = 0;
+        }
+        acc[countryISO3]++;
+        return acc;
+       }, {});
+
+       const formattedLocations = Object.entries(mappedLocations).map(
+        ([country, count]) => {
+            return { id: country, value: count }
+        }
+       );
+
+       res.status(200).json(formattedLocations);
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+};
