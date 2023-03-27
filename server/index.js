c944e4add9db893ec73d937254b5fb733321a71f
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-27 19:29:00 +0100

    modified the routes and endpoints for different files

--- server/index.js
+++ server/index.js
@@ -16,12 +16,15 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
+
 import { 
     dataUser, 
     dataProduct, 
     dataProductStat, 
     dataTransaction, 
     dataOverallStat,
+    dataAffiliateStat,
 } from "./data/index.js";
 
 // CONFIGURATION
@@ -55,4 +58,5 @@ mongoose
         // Transaction.insertMany(dataTransaction);
         // User.insertMany(dataUser)
         // OverallStat.insertMany(dataOverallStat);
+        // AffiliateStat.insertMany(dataAffiliateStat);
     }).catch((error) => console.log(`${error} did not connect`));
