84dc434ba146c078be6dccee0493dc5c71578f02
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-03-28 14:45:09 +0100

    React component that renders a dashboard with some statistics and charts related to sales

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,7 +1,192 @@
-import React from 'react'
+import React from 'react';
+import FlexBetween from 'components/FlexBetween';
+import Header from 'components/Header';
+import { DownloadOutlined, Email, PointOfSale, PersonAdd, Traffic } from '@mui/icons-material';
+import { Box, Typography, useTheme, useMediaQuery, Button } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import BreakdownChart from 'components/BreakdownChart';
+import OverViewChart from 'components/OverViewChart';
+import { useGetDashboardQuery } from 'state/api';
+import StatBox from "components/StatBox"
+
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  const theme = useTheme(); 
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const {data, isLoading} = useGetDashboardQuery();
+  
+
+  const columns = [
+    {
+        field: "_id",
+        headerName: "ID",
+        flex: 1,
+    },
+    {
+        field: "userId",
+        headerName: "User ID",
+        flex: 1,
+    },
+    {
+        field: "createdAt",
+        headerName: "createdAt",
+        flex: 1,
+    },
+    {
+        field: "products",
+        headerName: "# of Products",
+        flex: 0.5,
+        sortable: false,
+        renderCell: (params) => params.value.length
+    },
+    {
+        field: "cost",
+        headerName: "Cost",
+        flex: 1,
+        renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px"
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px"}} />
+            Download Reports
+          </Button>
+        </Box>
+      </FlexBetween>
+
+      {/* GRID */}
+      <Box 
+        mt="20px" 
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="160px"
+        gap="20px"
+        sx={{
+          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" }
+        }}
+      >
+        {/* ROW 1 */}
+        <StatBox
+          title="Total Customers"
+          value={data && data.totalCustomers}
+          increase="+20%"
+          description= "Since Last Month"
+          icon={
+            <Email sx={{ color: theme.palette.secondary[300] }} />
+          }
+        />
+
+        <StatBox
+          title="Sales Today"
+          value={data && data.todayStats.totalSales}
+          increase="+15%"
+          description= "Since Last Month"
+          icon={
+            <PointOfSale sx={{ color: theme.palette.secondary[300] }} />
+          }
+        />
+        
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius="0.55rem"
+        >
+          <OverViewChart view="sales" isDashboard={true} />
+        </Box>
+
+        <StatBox
+          title="Monthly Sales"
+          value={data && data.thisMonthStats.totalSales}
+          increase="+5%"
+          description= "Since Last Month"
+          icon={
+            <PersonAdd sx={{ color: theme.palette.secondary[300] }} />
+          }
+        />
+
+        <StatBox
+          title="Yearly Sales"
+          value={data && data.yearlySalesTotal}
+          increase="+43%"
+          description= "Since Last Month"
+          icon={
+            <Traffic sx={{ color: theme.palette.secondary[300] }} />
+          }
+        />
+
+                  
+          {/* ROW 2 */}
+          <Box 
+            gridColumn="span 8"
+            gridRow="span 3"
+            sx={{
+              "& .MuiDataGrid-root": {
+                  border: "none",
+                  borderRadius: "5rem"
+              },
+              "& .MuiDataGrid-cell": {
+                  borderBottom: "none"
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                  backgroundColor: theme.palette.background.alt,
+                  color: theme.palette.secondary[100],
+                  borderBottom: "none"
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                  backgroundColor: theme.palette.background.alt,
+              },
+              "& .MuiDataGrid-footerContainer": {
+                  backgroundColor: theme.palette.background.alt,
+                  color: theme.palette.secondary[100],
+                  borderTop: "none"
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                  color: `${theme.palette.secondary[200]} !important`,
+                  borderTop: "none"
+              },
+          }}
+          >
+            <DataGrid 
+              loading={isLoading || !data}
+              getRowId={(row) => row._id}
+              rows={(data && data.transactions) || []}
+              columns={columns}
+            />
+          </Box>
+          <Box
+            gridColumn="span 4"
+            gridRow="span 3"
+            backgroundColor={theme.palette.background.alt}
+            p="1.5rem"
+            borderRadius="0.55rem"
+          >
+            <Typography variant="h4" sx={{ color: theme.palette.secondary[100]} }> 
+              Sales By Category
+            </Typography>
+            <BreakdownChart isDashboard={true} />
+            <Typography p="0 0.6rem" fontSize="0.8rem" sx={{ color: theme.palette.secondary[200] }}>
+              Breakdown of real states and information via category for revenue made for this year and Total Sales
+            </Typography> 
+          </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
